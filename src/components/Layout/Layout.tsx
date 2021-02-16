ed591856bd75675285d0628e292f6d3130f256fa
sebalr <larrieu.sebastian@gmail.com>
2021-02-16 16:10:40 -0300

    Add layout and toolbar skeleton

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Toolbar from 'components/Layout/Toolbar/Toolbar';
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      <Toolbar />
+      <main>
+        <div>main</div>
+      </main>
+    </div>
+  )
+}
+
+export default Layout
